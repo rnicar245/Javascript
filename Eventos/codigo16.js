a73afd4f2f4a4122a4628bb6a361620590e03f49
rnicar245 <55734812+rnicar245@users.noreply.github.com>
2019-10-21 09:27:21 +0200

    Add files via upload

--- Eventos/codigo16.js
+++ Eventos/codigo16.js
@@ -1,15 +1,15 @@
 function informacion(elEvento) {
-  var evento = elEvento || window.event;
+  let evento = elEvento;
 
-  var coordenadaX = evento.clientX;
-  var coordenadaY = evento.clientY;
+  let coordenadaX = evento.clientX;
+  let coordenadaY = evento.clientY;
 
-  var dimensiones = tamanoVentanaNavegador();
-  var tamanoX = dimensiones[0];
-  var tamanoY = dimensiones[1];
+  let dimensiones = tamanoVentanaNavegador();
+  let tamanoX = dimensiones[0];
+  let tamanoY = dimensiones[1];
 
-  var posicionHorizontal = "";
-  var posicionVertical = "";
+  let posicionHorizontal = "";
+  let posicionVertical = "";
 
   if(coordenadaX > tamanoX/2) {
     posicionHorizontal = "derecha";
@@ -30,23 +30,16 @@ function informacion(elEvento) {
 
 function muestraInformacion(mensaje) {
   document.getElementById("info").innerHTML = '<h1>'+mensaje[0]+'</h1>';
-  for(var i=1; i<mensaje.length; i++) {
+  for(let i=1; i<mensaje.length; i++) {
     document.getElementById("info").innerHTML += '<p>'+mensaje[i]+'</p>';
   }
 }
 
 function tamanoVentanaNavegador(){
-  var dimensiones = [];
+  let dimensiones = [];
 
   if(typeof(window.innerWidth) == 'number') {
-    // No es IE
     dimensiones = [window.innerWidth, window.innerHeight];
-  } else if(document.documentElement && (document.documentElement.clientWidth || document.documentElement.clientHeight)) {
-    //IE 6 en modo estandar (no quirks)
-    dimensiones = [document.documentElement.clientWidth, document.documentElement.clientHeight];
-  } else if(document.body && (document.body.clientWidth || document.body.clientHeight)) {
-    //IE en modo quirks
-    dimensiones = [document.body.clientWidth, document.body.clientHeight];
   }
 
   return dimensiones;
